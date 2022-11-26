79284eca3f09854a01c43c81408894fcb38ee6d2
danieljhkim <danieljhkim1@gmail.com>
2022-11-26 15:05:55 -0500

    implement chart-trigger

---
+++ chart-trigger/src/index.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+exports.handler = async (event, context) => {
+  console.log('Received event:', JSON.stringify(event, null, 2));
+  let payload = {};
+  let statusCode = 200;
+  const headers = {
+    'Content-Type': 'application/json',
+    "Access-Control-Allow-Headers" : "Content-Type",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+  };
+  const data = {
+    action: "STORE_COIN_CHARTS",
+  }
+  const api_url = process.env['LAMBDA_URL'];
+  try {
+    const resp = await axios.post(api_url, JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+    });
+    payload.data = "success";
+  } catch(e) {
+    console.log(e.message);
+    payload.message = e.message;
+    statusCode = 500;
+  }
+  const finalResponse = {
+    statusCode: statusCode,
+    body: JSON.stringify(payload),
+    headers: headers
+  };
+  console.log("======finalResponse====>" + JSON.stringify(finalResponse));
+  return finalResponse;
+};
